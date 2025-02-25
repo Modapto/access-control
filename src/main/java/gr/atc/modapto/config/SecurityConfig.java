5754cd2ebef77409ea9ed0816423cf57509f6a2b
AlkAzna <a.aznavouridis@atc.gr>
2025-02-25 10:46:03 +0200

    Minor addition for CORS

--- src/main/java/gr/atc/modapto/config/SecurityConfig.java
+++ src/main/java/gr/atc/modapto/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package gr.atc.modapto.config;
 
 import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -26,78 +28,84 @@ import gr.atc.modapto.keycloak.UnauthorizedEntryPoint;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-        /**
-         * Initialize and Configure Security Filter Chain of HTTP connection
-         * 
-         * @param http       HttpSecurity
-         * @param entryPoint UnauthorizedEntryPoint -> To add proper API Response to the
-         *                   authorized request
-         * @return SecurityFilterChain
-         */
-        @Bean
-        public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, UnauthorizedEntryPoint entryPoint)
-                        throws Exception {
-                CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-                requestHandler.setCsrfRequestAttributeName("_csrf");
+  @Value("${spring.security.cors.domains}")
+  private List<String> corsDomains;
 
-                // Convert Keycloak Roles with class to Spring Security Roles
-                JwtAuthConverter jwtAuthConverter = new JwtAuthConverter();
+  /**
+   * Initialize and Configure Security Filter Chain of HTTP connection
+   * 
+   * @param http HttpSecurity
+   * @param entryPoint UnauthorizedEntryPoint -> To add proper API Response to the authorized
+   *        request
+   * @return SecurityFilterChain
+   */
+  @Bean
+  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
+      UnauthorizedEntryPoint entryPoint) throws Exception {
+    CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+    requestHandler.setCsrfRequestAttributeName("_csrf");
 
-                // Set Session to Stateless so not to keep any information about the JWT
-                http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                // Configure CORS access
-                                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
-                                // Configure CSRF Token
-                                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler)
-                                                .ignoringRequestMatchers("/api/users/**", "/api/admin/**", "/api/user-manager/**") // For now ignore all requests under api/users, and api/admin
-                                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                                .exceptionHandling(exc -> exc.authenticationEntryPoint(entryPoint))
-                                // HTTP Requests authorization properties on URLs
-                                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                                .requestMatchers("/api/users/authenticate", "/api/users/refresh-token", "/api/users/activate", "/api/users/reset-password", "/api/users/forgot-password", "/api/user-manager/**").permitAll()
-                                                .anyRequest().authenticated())
-                                // JWT Authentication Configuration to use with Keycloak
-                                .oauth2ResourceServer(oauth2ResourceServerCustomizer -> oauth2ResourceServerCustomizer
-                                        .jwt(jwtCustomizer -> jwtCustomizer.jwtAuthenticationConverter(jwtAuthConverter)));
-                return http.build();
-        }
+    // Convert Keycloak Roles with class to Spring Security Roles
+    JwtAuthConverter jwtAuthConverter = new JwtAuthConverter();
 
-        /**
-         * Initialize Granted Authorities Bean
-         * 
-         * @return GrantedAuthorityDefaults
-         */
-        @Bean
-        public GrantedAuthorityDefaults grantedAuthorityDefaults() {
-                return new GrantedAuthorityDefaults("");
-        }
+    // Set Session to Stateless so not to keep any information about the JWT
+    http.sessionManagement(
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // Configure CORS access
+        .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
+        // Configure CSRF Token
+        .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler)
+            .ignoringRequestMatchers("/api/users/**", "/api/admin/**", "/api/user-manager/**") // For
+                                                                                               // now
+                                                                                               // ignore
+                                                                                               // all
+                                                                                               // requests
+                                                                                               // under
+                                                                                               // api/users,
+                                                                                               // and
+                                                                                               // api/admin
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .exceptionHandling(exc -> exc.authenticationEntryPoint(entryPoint))
+        // HTTP Requests authorization properties on URLs
+        .authorizeHttpRequests(
+            authorizeRequests -> authorizeRequests
+                .requestMatchers("/api/users/authenticate", "/api/users/refresh-token",
+                    "/api/users/activate", "/api/users/reset-password",
+                    "/api/users/forgot-password", "/api/user-manager/**")
+                .permitAll().anyRequest().authenticated())
+        // JWT Authentication Configuration to use with Keycloak
+        .oauth2ResourceServer(oauth2ResourceServerCustomizer -> oauth2ResourceServerCustomizer
+            .jwt(jwtCustomizer -> jwtCustomizer.jwtAuthenticationConverter(jwtAuthConverter)));
+    return http.build();
+  }
 
-        /**
-         * Settings for CORS
-         * 
-         * @return CorsConfigurationSource
-         */
-        /**
-         * Settings for CORS
-         *
-         * @return CorsConfigurationSource
-         */
-        @Bean
-        public CorsConfigurationSource corsConfigurationSource() {
-                CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(Arrays.asList(
-                        "http://localhost:3000",
-                        "https://dashboard.modapto.atc.gr",
-                        "https://services.modapto.atc.gr",
-                        "https://keycloak.modapto.atc.gr"));
-                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                configuration.setAllowedHeaders(Arrays.asList("*"));
-                configuration.setAllowCredentials(true);
-                configuration.setMaxAge(86400L);
-                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", configuration);
-                return source;
-        }
+  /**
+   * Initialize Granted Authorities Bean
+   * 
+   * @return GrantedAuthorityDefaults
+   */
+  @Bean
+  public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+    return new GrantedAuthorityDefaults("");
+  }
+
+  /**
+   * Settings for CORS
+   *
+   * @return CorsConfigurationSource
+   */
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(corsDomains);
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+    configuration.setMaxAge(86400L);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
 }
